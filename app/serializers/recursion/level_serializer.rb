addb7745ced046350dcbaea9fc038212f6687c2c
Kaoru Kohashigawa <dev@kaoruk.com>
2015-11-27 09:02:34 -0800

    mispelled nil, really...

--- app/serializers/recursion/level_serializer.rb
+++ app/serializers/recursion/level_serializer.rb
@@ -11,7 +11,7 @@ module Recursion
     def meta
       {
         total_count: object.count,
-        marker: object.marker ? FriendSerializer.new(object.marker, root: false) : nill
+        marker: object.marker ? FriendSerializer.new(object.marker, root: false) : nil
       }
     end
 
